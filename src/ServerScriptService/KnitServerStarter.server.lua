5d87cc2843a8fd9ef4049162a6f31003703f7c2f
Jacob Isham <24721341+jwisham@users.noreply.github.com>
2022-04-18 03:41:40 -0500

    Create Knit Loaders

--- src/ServerScriptService/KnitServerStarter.server.lua
+++ src/ServerScriptService/KnitServerStarter.server.lua
@@ -1,1 +1,12 @@
-print("Server")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
+
+local Knit = require(ReplicatedStorage.Packages.Knit)
+
+for _, m in pairs(ServerScriptService.Services:GetChildren()) do
+    if m:IsA("ModuleScript") then
+        require(m)
+    end
+end
+
+Knit.Start():catch(warn)
--- src/StarterPlayerScripts/KnitClientStarter.client.lua
+++ src/StarterPlayerScripts/KnitClientStarter.client.lua
@@ -1,1 +1,11 @@
-print("Client")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Knit = require(ReplicatedStorage.Packages.Knit)
+
+for _, m in pairs(ReplicatedStorage.Controllers:GetChildren()) do
+    if m:IsA("ModuleScript") then
+        require(m)
+    end
+end
+
+Knit.Start():catch(warn)
